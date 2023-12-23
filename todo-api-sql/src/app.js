ffb4ab54b2528e0ea15e58eaa2b360731cdf7f55
Christopher Ramos <chrisandre23@gmail.com>
2023-12-23 18:02:23 -0500

    cambios ok

--- todo-api-sql/src/app.js
+++ todo-api-sql/src/app.js
@@ -4,15 +4,12 @@ import * as dotenv from 'dotenv'
 import routes from "./routes/index.routes.js";
 dotenv.config()
 const app = express()
-const corsOptions = {
-    origin: ['http://127.0.0.1:5174', 'https://todo-app-mu-orpin.vercel.app'] // Asegúrate de cambiar esto a la URL de tu front-end
-};
 
 // Habilita CORS con las opciones
 app.listen(process.env.PORT, () => {
     console.log('desde el puerto ' + process.env.PORT);
 })
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(routes)
