b2c0010bb460c9623309b3fcf59905bdc1899602
Christopher Ramos <chrisandre23@gmail.com>
2023-12-23 16:22:30 -0500

    cambios

--- todo-api-sql/src/database/bd.js
+++ todo-api-sql/src/database/bd.js
@@ -4,7 +4,7 @@ const pool = createPool({
     host: process.env.BD_SERVER,
     user: process.env.BD_USER,
     password: process.env.BD_PASSWORD,
-    port: 3306,
+    port: process.env.DB_PORT,
     database: process.env.BD_NAME
 })
 const BD = {
